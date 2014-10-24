9446bf83f9ed2f994afd2b618ed3654771338fe0
vivianlam <vivianlam10497@gmail.com>
2014-10-24 09:56:04 -0700

    yeeeahhhaaa

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 
 public void setup()
 {
@@ -69,7 +85,7 @@ public void setup()
  	public Chick(String type, String sound1, String sound2)
  	{
  		myType=type;
- 		if(Math.random()<0.5)
+ 		if(Math.random()<0.5f)
  		mySound=sound1;
  		else 
  		mySound=sound2;
@@ -100,3 +116,12 @@ public void setup()
   	public String getSound(){return mySound;}
   	public String getType(){return myType;}
   }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
